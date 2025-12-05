99d942018634f5926009b2e07a3b57a207b32a4e
carsonSgit <carsonspriggs6@gmail.com>
2025-12-05 14:40:44 -0500

    Remove unused files, add useful cleanup deps

--- workout_frontend/workout_user_frontend/src/components/Footer.js
+++
@@ -1,7 +0,0 @@
-function Footer(){
-    return(
-        <h1>Noah Groleau Copyright 2023</h1>
-    );
-}
-
-export default Footer;
--- workout_frontend/workout_user_frontend/src/pages/Contact.js
+++
@@ -1,7 +0,0 @@
-function Contact(){
-    return(
-        <h1>Chat with us on the forum!</h1>
-    );
-}
-
-export default Contact;
